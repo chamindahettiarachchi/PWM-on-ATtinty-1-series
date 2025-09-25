22ae50fbddfb5427f73ed3de822a7c02bcfbf450
chamindahettiarachchi <chamindahettiarachchi1981@gmail.com>
2025-09-25 06:46:19 +0500

    Update normal resolution.c

--- normal resolution.c
+++ normal resolution.c
@@ -4,21 +4,20 @@
 
 
 int main(void){
-    PORTB.DIRSET = PIN0_bm;                                // PB0 = output (WO0)
+    PORTB.DIRSET = PIN0_bm | PIN1_bm | PIN2_bm;//three pins can be set as output                               
 
-    TCA0.SINGLE.CTRLB = TCA_SINGLE_WGMODE_SINGLESLOPE_gc    // single-slope PWM
-                      | TCA_SINGLE_CMP0EN_bm;               // enable WO0
-    TCA0.SINGLE.PER  = 255;                                 // PWM period  (~203 Hz @ 3.333 MHz, /64)
-    TCA0.SINGLE.CMP0 = 128;                                 // ~50% duty
-    TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV64_gc          // prescaler /64
-                      | TCA_SINGLE_ENABLE_bm;               // start
+    TCA0.SINGLE.CTRLB = TCA_SINGLE_WGMODE_SINGLESLOPE_gc   
+                      | TCA_SINGLE_CMP0EN_bm   | TCA_SINGLE_CMP1EN_bm | TCA_SINGLE_CMP2EN_bm ;  //enable all three outputs as pwm           
+    TCA0.SINGLE.PER  = 255;                                 
+                                  
+    TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1_gc     //prescaler     
+                      | TCA_SINGLE_ENABLE_bm;               
 
-    while(1){ /* change duty by writing TCA0.SINGLE.CMP0 (0..255) */
-    
-        for(uint8_t x = 0; x < 256; x++){
-             TCA0.SINGLE.CMP0 = x; 
-             _delay_ms(7);
-             
-        }
+    while(1){ 
+        
+             TCA0.SINGLE.CMP1 = 50; // dutycycle can be adjusted seperatly 
+              TCA0.SINGLE.CMP2 = 125; 
+              TCA0.SINGLE.CMP0 = 255; 
+        
     }
 }
